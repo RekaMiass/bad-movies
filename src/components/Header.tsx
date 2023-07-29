4c69f417493cf6bf71a72754f496ea477f993cf4
RekaMiass <mariavram@mail.ru>
2023-07-29 17:37:06 +0500

    header layout

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <div className="container mx-auto flex items-center gap-x-5 px-40 py-4">
+            <img className="h-20 w-20" src="/icon.png" alt="" />
+            <h1 className="text-5xl font-bold uppercase">Киновтопку</h1>
+        </div>
+    )
+}
